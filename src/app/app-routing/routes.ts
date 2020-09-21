02b10c91b8a1d47c98b78e1cba65219faca88dab
Sunnymaharshi <maharshireddythangasani@gmail.com>
2020-09-21 17:35:30 +0530

    assi 2 completed

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -11,5 +11,7 @@ export const routes: Routes = [
   { path: 'menu',     component: MenuComponent },
   { path: 'contactus',     component: ContactComponent },
   { path: 'dishdetail/:id',     component: DishdetailComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'aboutus', component: AboutComponent}
+
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,9 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { LeaderService } from './services/leader.service';
 import { PromotionService } from './services/promotion.service';
+
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
 
@@ -47,7 +49,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
   ],
   providers: [ 
     DishService,
-    PromotionService
+    PromotionService,
+    LeaderService
   ],
   bootstrap: [AppComponent]
 })
